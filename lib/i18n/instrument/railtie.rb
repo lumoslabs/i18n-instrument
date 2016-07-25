f56dd872f12ae499856adbff45119e42ec687224
Cameron Dutro <camertron@gmail.com>
2016-07-25 12:06:13 -0700

    Add gem's assets to rails asset path

--- lib/i18n/instrument/railtie.rb
+++ lib/i18n/instrument/railtie.rb
@@ -2,6 +2,7 @@ module I18n
   module Instrument
     class Railtie < Rails::Railtie
       initializer 'i18n.instrument.middleware' do |app|
+        app.config.assets.paths << File.expand_path('../../../assets/javascripts', __FILE__)
         app.middleware.use(I18n::Instrument::Middleware)
       end
     end
